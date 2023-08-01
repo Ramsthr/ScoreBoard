f314509556cf2450ce57040af822a2c61006c346
lokesh1383108 <786sharmallll@gmail.com>
2023-08-01 21:54:07 +0530

    HTml,css ,js file changed

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ btnReset.addEventListener('click', () => {
   curValue = 0;
   mainTitle.textContent = curValue;
 });
+
+const mainTitle1 = document.querySelector('#title1');
+let curValue1 = 0;
+
+const btnDecrement1 = document.querySelector('#decrement1');
+const btnReset1 = document.querySelector('#reset1');
+const btnIncrement1 = document.querySelector('#increment1');
+
+btnIncrement1.addEventListener('click', () => {
+  curValue1++;
+  mainTitle1.textContent = curValue1;
+});
+
+btnDecrement1.addEventListener('click', () => {
+  curValue1--;
+  mainTitle1.textContent = curValue1;
+});
+
+btnReset1.addEventListener('click', () => {
+  curValue1 = 0;
+  mainTitle1.textContent = curValue1;
+});
